641f095fcb1c945608b9e4ad58333759b9d45ced
ertkonsert <martaaagren@gmail.com>
2020-09-23 10:43:24 +0200

    m

---
+++ saving grejs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace saving_grejs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
